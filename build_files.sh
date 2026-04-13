5278c489a53a44fc52dd732f0d69c21e6660da0c
Arsh Goel <arshgoel16@gmail.com>
2026-04-13 21:15:50 +0530

    5

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
 echo "BUILD START"
-pip install requirements.txt
+python -m venv venv
+source venv/bin/activate
+
+pip install -r requirements.txt
 python manage.py makemigrations 
 python manage.py migrate
 python manage.py collectstatic --noinput --clear
